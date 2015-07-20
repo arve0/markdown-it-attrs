bf8d9eb91a59b00a5d2af4796c60a67f729b1eb4
arve0 <arve.seljebu@gmail.com>
2015-07-20 18:15:53 +0200

    add tests

--- test.js
+++ test.js
@@ -1,7 +1,40 @@
 'use strict';
 var assert = require('assert');
+var Md = require('markdown-it');
 var markdownItAttrs = require('./');
 
-it('should ', function () {
-	assert.strictEqual(markdownItAttrs('unicorns'), 'unicorns & rainbows');
+describe('markdown-it-attrs', function() {
+  var md;
+  beforeEach(function(){
+    md = Md().use(markdownItAttrs);
+  });
+
+  it('should add attributes when {} in end of last inline', function () {
+    var src = 'some text {with=attrs}';
+    var expected = '<p with="attrs">some text</p>\n';
+    var res = md.render(src);
+    assert.equal(expected, res);
+  });
+
+  it('should add attributes when {} in last line', function () {
+    var src = 'some text\n{with=attrs}';
+    var expected = '<p with="attrs">some text\n</p>\n';
+    var res = md.render(src);
+    assert.equal(expected, res);
+  });
+
+  it('should add classes with {.class} dot notation', function () {
+    var src = 'some text {.green}';
+    var expected = '<p class="green">some text</p>\n';
+    var res = md.render(src);
+    assert.equal(expected, res);
+  });
+
+  it('should add identifiers with {#id} hashtag notation', function () {
+    var src = 'some text {#section2}';
+    var expected = '<p id="section2">some text</p>\n';
+    var res = md.render(src);
+    assert.equal(expected, res);
+  });
+
 });
