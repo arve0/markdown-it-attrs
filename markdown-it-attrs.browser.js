bcc36b13efe61ff23cea009ae7e4e10c8e94b142
Arve Seljebu <arve.seljebu@gmail.com>
2019-04-22 18:11:06 +0200

    2.3.3

--- markdown-it-attrs.browser.js
+++ markdown-it-attrs.browser.js
@@ -1,7 +1,7 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.markdownItAttrs = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 'use strict';
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 var patternsConfig = require('./patterns.js');
 
@@ -24,15 +24,20 @@ module.exports = function attributes(md, options) {
       for (var p = 0; p < patterns.length; p++) {
         var pattern = patterns[p];
         var j = null; // position of child with offset 0
+
         var match = pattern.tests.every(function (t) {
           var res = test(tokens, i, t);
+
           if (res.j !== null) {
             j = res.j;
           }
+
           return res.match;
         });
+
         if (match) {
           pattern.transform(tokens, i, j);
+
           if (pattern.name === 'inline attributes' || pattern.name === 'inline nesting 0') {
             // retry, may be several inline attributes
             p--;
@@ -48,7 +53,6 @@ module.exports = function attributes(md, options) {
 
   md.core.ruler.before('linkify', 'curly_attributes', curlyAttrs);
 };
-
 /**
  * Test if t matches token stream.
  *
@@ -57,15 +61,16 @@ module.exports = function attributes(md, options) {
  * @param {object} t Test to match.
  * @return {object} { match: true|false, j: null|number }
  */
+
+
 function test(tokens, i, t) {
   var res = {
     match: false,
     j: null // position of child
-  };
 
+  };
   var ii = t.shift !== undefined ? i + t.shift : t.position;
   var token = get(tokens, ii); // supports negative ii
-
 
   if (token === undefined) {
     return res;
@@ -73,7 +78,7 @@ function test(tokens, i, t) {
 
   var _loop2 = function _loop2(key) {
     if (key === 'shift' || key === 'position') {
-      return 'continue';
+      return "continue";
     }
 
     if (token[key] === undefined) {
@@ -83,7 +88,7 @@ function test(tokens, i, t) {
     }
 
     if (key === 'children' && isArrayOfObjects(t.children)) {
-      var _ret3 = function () {
+      var _ret2 = function () {
         if (token.children.length === 0) {
           return {
             v: {
@@ -91,9 +96,11 @@ function test(tokens, i, t) {
             }
           };
         }
-        var match = void 0;
+
+        var match;
         var childTests = t.children;
         var children = token.children;
+
         if (childTests.every(function (tt) {
           return tt.position !== undefined;
         })) {
@@ -101,6 +108,7 @@ function test(tokens, i, t) {
           match = childTests.every(function (tt) {
             return test(children, tt.position, tt).match;
           });
+
           if (match) {
             // we may need position of child in transform
             var j = last(childTests).position;
@@ -111,17 +119,18 @@ function test(tokens, i, t) {
             match = childTests.every(function (tt) {
               return test(children, _j, tt).match;
             });
+
             if (match) {
-              res.j = _j;
-              // all tests true, continue with next key of pattern t
-              return 'break';
+              res.j = _j; // all tests true, continue with next key of pattern t
+
+              return "break";
             }
           };
 
           for (var _j = 0; _j < children.length; _j++) {
-            var _ret4 = _loop3(_j);
+            var _ret3 = _loop3(_j);
 
-            if (_ret4 === 'break') break;
+            if (_ret3 === "break") break;
           }
         }
 
@@ -134,11 +143,11 @@ function test(tokens, i, t) {
         }
 
         return {
-          v: 'continue'
+          v: "continue"
         };
       }();
 
-      if ((typeof _ret3 === 'undefined' ? 'undefined' : _typeof(_ret3)) === "object") return _ret3.v;
+      if (_typeof(_ret2) === "object") return _ret2.v;
     }
 
     switch (_typeof(t[key])) {
@@ -150,52 +159,60 @@ function test(tokens, i, t) {
             v: res
           };
         }
+
         break;
+
       case 'function':
         if (!t[key](token[key])) {
           return {
             v: res
           };
         }
+
         break;
+
       case 'object':
         if (isArrayOfFunctions(t[key])) {
           var r = t[key].every(function (tt) {
             return tt(token[key]);
           });
+
           if (r === false) {
             return {
               v: res
             };
           }
+
           break;
         }
+
       // fall through for objects !== arrays of functions
+
       default:
-        throw new Error('Unknown type of pattern test (key: ' + key + '). Test should be of type boolean, number, string, function or array of functions.');
+        throw new Error("Unknown type of pattern test (key: ".concat(key, "). Test should be of type boolean, number, string, function or array of functions."));
     }
   };
 
   for (var key in t) {
-    var _ret2 = _loop2(key);
+    var _ret = _loop2(key);
 
-    switch (_ret2) {
-      case 'continue':
+    switch (_ret) {
+      case "continue":
         continue;
 
       default:
-        if ((typeof _ret2 === 'undefined' ? 'undefined' : _typeof(_ret2)) === "object") return _ret2.v;
+        if (_typeof(_ret) === "object") return _ret.v;
     }
-  }
+  } // no tests returned false -> all tests returns true
 
-  // no tests returned false -> all tests returns true
+
   res.match = true;
   return res;
 }
 
 function isArrayOfObjects(arr) {
   return Array.isArray(arr) && arr.length && arr.every(function (i) {
-    return (typeof i === 'undefined' ? 'undefined' : _typeof(i)) === 'object';
+    return _typeof(i) === 'object';
   });
 }
 
@@ -204,18 +221,19 @@ function isArrayOfFunctions(arr) {
     return typeof i === 'function';
   });
 }
-
 /**
  * Get n item of array. Supports negative n, where -1 is last
  * element in array.
  * @param {array} arr
  * @param {number} n
  */
+
+
 function get(arr, n) {
   return n >= 0 ? arr[n] : arr[arr.length + n];
-}
+} // get last element of array, safe - returns {} if not found
 
-// get last element of array, safe - returns {} if not found
+
 function last(arr) {
   return arr.slice(-1)[0] || {};
 }
@@ -280,6 +298,7 @@ module.exports = function (options) {
       var attrToken = tokens[i].children[j - 1];
       var attrs = utils.getAttrs(token.content, 0, options);
       utils.addAttrs(attrs, attrToken);
+
       if (token.content.length === endChar + options.rightDelimiter.length) {
         tokens[i].children.splice(j, 1);
       } else {
@@ -310,10 +329,10 @@ module.exports = function (options) {
     transform: function transform(tokens, i) {
       var token = tokens[i + 2];
       var tableOpen = utils.getMatchingOpeningToken(tokens, i);
-      var attrs = utils.getAttrs(token.content, 0, options);
-      // add attributes
-      utils.addAttrs(attrs, tableOpen);
-      // remove <p>{.c}</p>
+      var attrs = utils.getAttrs(token.content, 0, options); // add attributes
+
+      utils.addAttrs(attrs, tableOpen); // remove <p>{.c}</p>
+
       tokens.splice(i + 1, 3);
     }
   }, {
@@ -327,6 +346,7 @@ module.exports = function (options) {
       children: [{
         shift: -1,
         nesting: -1 // closing inline tag, </em>{.a}
+
       }, {
         shift: 0,
         type: 'text',
@@ -366,9 +386,11 @@ module.exports = function (options) {
       var content = token.content;
       var attrs = utils.getAttrs(content, 0, options);
       var ii = i - 2;
+
       while (tokens[ii - 1] && tokens[ii - 1].type !== 'ordered_list_open' && tokens[ii - 1].type !== 'bullet_list_open') {
         ii--;
       }
+
       utils.addAttrs(attrs, tokens[ii - 1]);
       tokens[i].children = tokens[i].children.slice(0, -2);
     }
@@ -454,12 +476,14 @@ module.exports = function (options) {
     }],
     transform: function transform(tokens, i, j) {
       var token = tokens[i].children[j];
-      var attrs = utils.getAttrs(token.content, 0, options);
-      // find last closing tag
+      var attrs = utils.getAttrs(token.content, 0, options); // find last closing tag
+
       var ii = i + 1;
+
       while (tokens[ii + 1] && tokens[ii + 1].nesting === -1) {
         ii++;
       }
+
       var openingToken = utils.getMatchingOpeningToken(tokens, ii);
       utils.addAttrs(attrs, openingToken);
       tokens[i].children = tokens[i].children.slice(0, -2);
@@ -517,18 +541,20 @@ module.exports = function (options) {
       var content = token.content;
       var attrs = utils.getAttrs(content, content.lastIndexOf(options.leftDelimiter), options);
       var ii = i + 1;
+
       while (tokens[ii + 1] && tokens[ii + 1].nesting === -1) {
         ii++;
       }
+
       var openingToken = utils.getMatchingOpeningToken(tokens, ii);
       utils.addAttrs(attrs, openingToken);
       var trimmed = content.slice(0, content.lastIndexOf(options.leftDelimiter));
       token.content = last(trimmed) !== ' ' ? trimmed : trimmed.slice(0, -1);
     }
   }];
-};
+}; // get last element of array or string
 
-// get last element of array or string
+
 function last(arr) {
   return arr.slice(-1)[0];
 }
@@ -549,32 +575,31 @@ exports.getAttrs = function (str, start, options) {
   var keySeparator = '=';
   var classChar = '.';
   var idChar = '#';
-
   var attrs = [];
   var key = '';
   var value = '';
   var parsingKey = true;
-  var valueInsideQuotes = false;
-
-  // read inside {}
+  var valueInsideQuotes = false; // read inside {}
   // start + left delimiter length to avoid beginning {
   // breaks when } is found or end of string
+
   for (var i = start + options.leftDelimiter.length; i < str.length; i++) {
     if (str.slice(i, i + options.rightDelimiter.length) === options.rightDelimiter) {
       if (key !== '') {
         attrs.push([key, value]);
       }
+
       break;
     }
-    var char_ = str.charAt(i);
 
-    // switch to reading value if equal sign
+    var char_ = str.charAt(i); // switch to reading value if equal sign
+
     if (char_ === keySeparator && parsingKey) {
       parsingKey = false;
       continue;
-    }
+    } // {.class} {..css-module}
 
-    // {.class} {..css-module}
+
     if (char_ === classChar && key === '') {
       if (str.charAt(i + 1) === classChar) {
         key = 'css-module';
@@ -582,64 +607,71 @@ exports.getAttrs = function (str, start, options) {
       } else {
         key = 'class';
       }
+
       parsingKey = false;
       continue;
-    }
+    } // {#id}
 
-    // {#id}
+
     if (char_ === idChar && key === '') {
       key = 'id';
       parsingKey = false;
       continue;
-    }
+    } // {value="inside quotes"}
 
-    // {value="inside quotes"}
+
     if (char_ === '"' && value === '') {
       valueInsideQuotes = true;
       continue;
     }
+
     if (char_ === '"' && valueInsideQuotes) {
       valueInsideQuotes = false;
       continue;
-    }
+    } // read next key/value pair
 
-    // read next key/value pair
+
     if (char_ === pairSeparator && !valueInsideQuotes) {
       if (key === '') {
         // beginning or ending space: { .red } vs {.red}
         continue;
       }
+
       attrs.push([key, value]);
       key = '';
       value = '';
       parsingKey = true;
       continue;
-    }
+    } // continue if character not allowed
 
-    // continue if character not allowed
+
     if (parsingKey && char_.search(allowedKeyChars) === -1) {
       continue;
-    }
+    } // no other conditions met; append to key/value
 
-    // no other conditions met; append to key/value
+
     if (parsingKey) {
       key += char_;
       continue;
     }
+
     value += char_;
   }
+
   return attrs;
 };
-
 /**
  * add attributes from [['key', 'val']] list
  * @param {array} attrs: [['key', 'val']]
  * @param {token} token: which token to add attributes
  * @returns token
  */
+
+
 exports.addAttrs = function (attrs, token) {
   for (var j = 0, l = attrs.length; j < l; ++j) {
     var key = attrs[j][0];
+
     if (key === 'class') {
       token.attrJoin('class', attrs[j][1]);
     } else if (key === 'css-module') {
@@ -648,9 +680,9 @@ exports.addAttrs = function (attrs, token) {
       token.attrPush(attrs[j]);
     }
   }
+
   return token;
 };
-
 /**
  * Does string have properly formatted curly?
  *
@@ -662,19 +694,22 @@ exports.addAttrs = function (attrs, token) {
  * @param {string} where to expect {} curly. start, middle, end or only.
  * @return {function(string)} Function which testes if string has curly.
  */
-exports.hasDelimiters = function (where, options) {
 
+
+exports.hasDelimiters = function (where, options) {
   if (!where) {
     throw new Error('Parameter `where` not passed. Should be "start", "middle", "end" or "only".');
   }
-
   /**
    * @param {string} str
    * @return {boolean}
    */
+
+
   return function (str) {
     // we need minimum three chars, for example {b}
     var minCurlyLength = options.leftDelimiter.length + 1 + options.rightDelimiter.length;
+
     if (!str || typeof str !== 'string' || str.length < minCurlyLength) {
       return false;
     }
@@ -685,22 +720,22 @@ exports.hasDelimiters = function (where, options) {
       return isClass || isId ? curly.length >= minCurlyLength + 1 : curly.length >= minCurlyLength;
     }
 
-    var start = void 0,
-        end = void 0,
-        slice = void 0,
-        nextChar = void 0;
+    var start, end, slice, nextChar;
     var rightDelimiterMinimumShift = minCurlyLength - options.rightDelimiter.length;
+
     switch (where) {
       case 'start':
         // first char should be {, } found in char 2 or more
         slice = str.slice(0, options.leftDelimiter.length);
         start = slice === options.leftDelimiter ? 0 : -1;
-        end = start === -1 ? -1 : str.indexOf(options.rightDelimiter, rightDelimiterMinimumShift);
-        // check if next character is not one of the delimiters
+        end = start === -1 ? -1 : str.indexOf(options.rightDelimiter, rightDelimiterMinimumShift); // check if next character is not one of the delimiters
+
         nextChar = str.charAt(end + options.rightDelimiter.length);
+
         if (nextChar && options.rightDelimiter.indexOf(nextChar) !== -1) {
           end = -1;
         }
+
         break;
 
       case 'end':
@@ -722,20 +757,18 @@ exports.hasDelimiters = function (where, options) {
     return start !== -1 && end !== -1 && validCurlyLength(str.substring(start, end + options.rightDelimiter.length));
   };
 };
-
 /**
  * Removes last curly from string.
  */
+
+
 exports.removeDelimiter = function (str, options) {
   var start = escapeRegExp(options.leftDelimiter);
   var end = escapeRegExp(options.rightDelimiter);
-
   var curly = new RegExp('[ \\n]?' + start + '[^' + start + end + ']+' + end + '$');
   var pos = str.search(curly);
-
   return pos !== -1 ? str.slice(0, pos) : str;
 };
-
 /**
  * Escapes special characters in string s such that the string
  * can be used in `new RegExp`. For example "[" becomes "\\[".
@@ -743,25 +776,29 @@ exports.removeDelimiter = function (str, options) {
  * @param {string} s Regex string.
  * @return {string} Escaped string.
  */
+
+
 function escapeRegExp(s) {
   return s.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
 }
-exports.escapeRegExp = escapeRegExp;
 
+exports.escapeRegExp = escapeRegExp;
 /**
  * find corresponding opening block
  */
+
 exports.getMatchingOpeningToken = function (tokens, i) {
   if (tokens[i].type === 'softbreak') {
     return false;
-  }
-  // non closing blocks, example img
+  } // non closing blocks, example img
+
+
   if (tokens[i].nesting === 0) {
     return tokens[i];
-  }
-
-  // inline tokens changes level on same token
+  } // inline tokens changes level on same token
   // that have .nesting +- 1
+
+
   var level = tokens[i].block ? tokens[i].level : tokens[i].level + 1; // adjust for inline tokens
 
   var type = tokens[i].type.replace('_close', '_open');
@@ -772,10 +809,11 @@ exports.getMatchingOpeningToken = function (tokens, i) {
     }
   }
 };
-
 /**
  * from https://github.com/markdown-it/markdown-it/blob/master/lib/common/utils.js
  */
+
+
 var HTML_ESCAPE_TEST_RE = /[&<>"]/;
 var HTML_ESCAPE_REPLACE_RE = /[&<>"]/g;
 var HTML_REPLACEMENTS = {
@@ -793,6 +831,7 @@ exports.escapeHtml = function (str) {
   if (HTML_ESCAPE_TEST_RE.test(str)) {
     return str.replace(HTML_ESCAPE_REPLACE_RE, replaceUnsafeChar);
   }
+
   return str;
 };
 
